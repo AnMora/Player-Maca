3c92f9e4cb33f250725f595fdd93aa7c3b48c2b4
Anmora96 <anmora.2396@gmail.com>
2021-02-17 07:01:47 +0000

    Player mejorado aun no terminado

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+//import { Article } from "react-bootstrap";
 
 //create your first Player
 export class Home extends React.Component {
@@ -40,82 +41,105 @@ export class Home extends React.Component {
 		this.setState({ currentIndex: i });
 	};
 
+	pause = () => {
+		this.audio.pause();
+	};
+
 	render() {
 		const audioPlayer = (
 			<>
-				<div id="player">
-					<img src="" className="cover" id="art" />
-					<div className="trackinfo">
-						{this.state.songs.map((song, index) => {
-							return (
-								<p
-									id="title"
-									key={index}
-									onClick={() =>
-										this.play(this.state.currentIndex)
-									}>
-									{song.name}
-								</p>
-							);
-						})}
-					</div>
-					<div className="audioPlayer">
-						<div className="info">
-							<div className="cont">
-								<button
-									id="play"
-									className="nav"
-									ref={element => (this.playButton = element)}
-									onClick={() =>
-										this.play(this.state.currentIndex)
-									}>
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										width="24"
-										height="24"
-										viewBox="0 0 24 24">
-										<path d="M3 22v-20l18 10-18 10z" />
-									</svg>
-								</button>
-								<button
-									id="prev"
-									className="nav"
-									onClick={() =>
-										this.play(this.state.currentIndex - 1)
-									}>
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										width="24"
-										height="24"
-										viewBox="0 0 24 24">
-										<path d="M4 2v20h-2v-20h2zm18 0l-16 10 16 10v-20z" />
-									</svg>
-								</button>
-
-								<button
-									id="play"
-									className="nav"
-									ref={element =>
-										(this.pauseButton = element)
-									}
-									onClick={() =>
-										this.pause(this.state.currentIndex)
-									}>
-									<img src="http://abarcarodriguez.com/lab/pause.png" />
-								</button>
-								<button
-									id="next"
-									className="nav"
-									onClick={() =>
-										this.play(this.state.currentIndex + 1)
-									}>
-									<img src="http://abarcarodriguez.com/lab/next.png" />
-								</button>
+				<div className="reproductor">
+					<div id="player">
+						<img src="" className="cover" id="art" />
+						<div className="trackinfo">
+							{this.state.songs.map((song, index) => {
+								return (
+									<p
+										id="title"
+										key={index}
+										onClick={() =>
+											this.play(this.state.currentIndex)
+										}>
+										{song.name}
+									</p>
+								);
+							})}
+						</div>
+						<div className="audioPlayer">
+							<div className="info">
+								<div className="cont">
+									<button
+										id="prev"
+										className="nave"
+										onClick={() =>
+											this.play(
+												this.state.currentIndex - 1
+											)
+										}>
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											width="30"
+											height="30"
+											viewBox="0 0 24 24">
+											<path d="M4 2v20h-2v-20h2zm18 0l-16 10 16 10v-20z" />
+										</svg>
+									</button>
+									<button
+										id="play"
+										className="nave"
+										ref={element =>
+											(this.playButton = element)
+										}
+										onClick={() =>
+											this.play(this.state.currentIndex)
+										}>
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											width="30"
+											height="30"
+											viewBox="0 0 24 24">
+											<path d="M3 22v-20l18 10-18 10z" />
+										</svg>
+									</button>
+									<button
+										id="pause"
+										className="nave"
+										ref={element =>
+											(this.pauseButton = element)
+										}
+										onClick={() =>
+											this.pause(this.state.currentIndex)
+										}>
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											width="30"
+											height="30"
+											viewBox="0 0 24 24">
+											<path d="M11 22h-4v-20h4v20zm6-20h-4v20h4v-20z" />
+										</svg>
+									</button>
+									<button
+										id="next"
+										className="nave"
+										onClick={() =>
+											this.play(
+												this.state.currentIndex + 1
+											)
+										}>
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											width="30"
+											height="30"
+											viewBox="0 0 24 24">
+											<path d="M19 12l-18 12v-24l18 12zm4-11h-4v22h4v-22z" />
+										</svg>
+									</button>
+								</div>
 							</div>
 						</div>
 					</div>
+					<audio ref={element => (this.audio = element)} />
 				</div>
-				<audio ref={element => (this.audio = element)} />
 			</>
 		);
 		return <>{audioPlayer}</>;
